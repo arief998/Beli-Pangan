7cf79f5708616d6ae7b24f851ced30acff227ea3
Farismusyaffa <18523110@students.uii.ac.id>
2020-12-08 11:26:28 +0700

    Menambahkan tampilan Edit Account Seller dan Buyer

---
+++ app/src/main/java/com/example/belipangan/edit_seller_account.java
@@ -0,0 +1,21 @@
+package com.example.belipangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class edit_seller_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_seller_account);
+    }
+
+    public void simpanEditSeller(View view) {
+    }
+
+    public void editFotoSeller(View view) {
+    }
+}
